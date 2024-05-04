27e97d5597bbf8d26d431de78c4835bf1564e10e
Caleb <calebwagner5@gmail.com>
2024-05-04 17:57:33 -0500

    added http method to controller

--- src/main/java/com/postgresql/sqldemo/controller/CourseController.java
+++ src/main/java/com/postgresql/sqldemo/controller/CourseController.java
@@ -3,9 +3,8 @@ package com.postgresql.sqldemo.controller;
 import com.postgresql.sqldemo.model.Course;
 import com.postgresql.sqldemo.repo.CourseRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CourseController {
@@ -13,8 +12,32 @@ public class CourseController {
     @Autowired
     private CourseRepo courseRepo;
 
+    @GetMapping("/getCourse/{id}")
+    public Course getCourse(@PathVariable Long id) {
+        return courseRepo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Course not found with id " + id));
+    }
+
     @PostMapping("/addCourse")
     public void addPerson(@RequestBody Course course) {
+        courseRepo.save(course);
+    }
+
+    @DeleteMapping("/deleteCourse/{id}")
+    public void deleteCourse(@PathVariable Long id) {
+        courseRepo.deleteById(id);
+    }
+
+    @PutMapping("/updateCourse/{id}")
+    public void updateCourse(@PathVariable Long id, @RequestBody Course updatedCourse) {
+        Course course = courseRepo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Course not found with id " + id));
+
+        course.setTitle(updatedCourse.getTitle());
+        course.setDuration(updatedCourse.getDuration());
+        course.setContentUrl(updatedCourse.getContentUrl());
+        course.setIsRetired(updatedCourse.getIsRetired());
+
         courseRepo.save(course);
     }
 }
